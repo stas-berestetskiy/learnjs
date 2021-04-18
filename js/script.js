d470395485495b128ab34384d447a22d6ef8c871
Stanislav Berestetskiy <stasberestetskiy@gmail.com>
2021-04-18 21:05:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Are you 18?', '');
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
+
+console.log(2*4 === '8');
+
+&& //AND
+|| //OR
+! //оператор отрицания - переварачивает значение на противоположное
+!= //not equal
